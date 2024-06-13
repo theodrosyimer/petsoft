d721a815bbbb0433403bf47066261832f80e2538
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-06-13 14:02:36 +0200

    chore: Refactor auth-edge.ts types

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -1,82 +1,82 @@
-import { NextAuthConfig } from "next-auth";
-import prisma from "./db";
+import { NextAuthConfig } from 'next-auth'
+import prisma from './db'
 
 export const nextAuthEdgeConfig = {
   pages: {
-    signIn: "/login",
+    signIn: '/login',
   },
   callbacks: {
     authorized: ({ auth, request }) => {
       // runs on every request with middleware
-      const isLoggedIn = Boolean(auth?.user);
-      const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
+      const isLoggedIn = Boolean(auth?.user)
+      const isTryingToAccessApp = request.nextUrl.pathname.includes('/app')
 
       if (!isLoggedIn && isTryingToAccessApp) {
-        return false;
+        return false
       }
 
       if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
-        return Response.redirect(new URL("/payment", request.nextUrl));
+        return Response.redirect(new URL('/payment', request.nextUrl))
       }
 
       if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
-        return true;
+        return true
       }
 
       if (
         isLoggedIn &&
-        (request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")) &&
+        (request.nextUrl.pathname.includes('/login') ||
+          request.nextUrl.pathname.includes('/signup')) &&
         auth?.user.hasAccess
       ) {
-        return Response.redirect(new URL("/app/dashboard", request.nextUrl));
+        return Response.redirect(new URL('/app/dashboard', request.nextUrl))
       }
 
       if (isLoggedIn && !isTryingToAccessApp && !auth?.user.hasAccess) {
         if (
-          request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")
+          request.nextUrl.pathname.includes('/login') ||
+          request.nextUrl.pathname.includes('/signup')
         ) {
-          return Response.redirect(new URL("/payment", request.nextUrl));
+          return Response.redirect(new URL('/payment', request.nextUrl))
         }
 
-        return true;
+        return true
       }
 
       if (!isLoggedIn && !isTryingToAccessApp) {
-        return true;
+        return true
       }
 
-      return false;
+      return false
     },
     jwt: async ({ token, user, trigger }) => {
       if (user) {
         // on sign in
-        token.userId = user.id;
-        token.email = user.email!;
-        token.hasAccess = user.hasAccess;
+        token.userId = user.id
+        token.email = user.email!
+        token.hasAccess = user.hasAccess
       }
 
-      if (trigger === "update") {
+      if (trigger === 'update') {
         // on every request
         const userFromDb = await prisma.user.findUnique({
           where: {
-            email: token.email,
+            email: token.email as string,
           },
-        });
+        })
         if (userFromDb) {
-          token.hasAccess = userFromDb.hasAccess;
+          token.hasAccess = userFromDb.hasAccess
         }
       }
 
-      return token;
+      return token
     },
     session: ({ session, token }) => {
-      session.user.id = token.userId;
-      session.user.hasAccess = token.hasAccess;
+      session.user.id = token.userId as string
+      session.user.hasAccess = token.hasAccess as boolean
 
-      return session;
+      return session
     },
   },
   providers: [],
-} satisfies NextAuthConfig;
+} satisfies NextAuthConfig
