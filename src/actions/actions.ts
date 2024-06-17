19b14e51162440d8f3642ac231dd7ce7df615210
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-06-17 14:34:18 +0200

    chore: Update price id

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,46 +1,46 @@
-"use server";
+'use server'
 
-import { auth, signIn, signOut } from "@/lib/auth-no-edge";
-import prisma from "@/lib/db";
-import { sleep } from "@/lib/utils";
-import { authSchema, petFormSchema, petIdSchema } from "@/lib/validations";
-import { revalidatePath } from "next/cache";
-import bcrypt from "bcryptjs";
-import { redirect } from "next/navigation";
-import { checkAuth, getPetById } from "@/lib/server-utils";
-import { Prisma } from "@prisma/client";
-import { AuthError } from "next-auth";
+import { auth, signIn, signOut } from '@/lib/auth-no-edge'
+import prisma from '@/lib/db'
+import { sleep } from '@/lib/utils'
+import { authSchema, petFormSchema, petIdSchema } from '@/lib/validations'
+import { revalidatePath } from 'next/cache'
+import bcrypt from 'bcryptjs'
+import { redirect } from 'next/navigation'
+import { checkAuth, getPetById } from '@/lib/server-utils'
+import { Prisma } from '@prisma/client'
+import { AuthError } from 'next-auth'
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // --- user actions ---
 
 export async function logIn(prevState: unknown, formData: unknown) {
   if (!(formData instanceof FormData)) {
     return {
-      message: "Invalid form data.",
-    };
+      message: 'Invalid form data.',
+    }
   }
 
   try {
-    await signIn("credentials", formData);
+    await signIn('credentials', formData)
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case "CredentialsSignin": {
+        case 'CredentialsSignin': {
           return {
-            message: "Invalid credentials.",
-          };
+            message: 'Invalid credentials.',
+          }
         }
         default: {
           return {
-            message: "Error. Could not sign in.",
-          };
+            message: 'Error. Could not sign in.',
+          }
         }
       }
     }
 
-    throw error; // nextjs redirects throws error, so we need to rethrow it
+    throw error // nextjs redirects throws error, so we need to rethrow it
   }
 }
 
@@ -48,61 +48,61 @@ export async function signUp(prevState: unknown, formData: unknown) {
   // check if formData is a FormData type
   if (!(formData instanceof FormData)) {
     return {
-      message: "Invalid form data.",
-    };
+      message: 'Invalid form data.',
+    }
   }
 
   // convert formData to a plain object
-  const formDataEntries = Object.fromEntries(formData.entries());
+  const formDataEntries = Object.fromEntries(formData.entries())
 
   // validation
-  const validatedFormData = authSchema.safeParse(formDataEntries);
+  const validatedFormData = authSchema.safeParse(formDataEntries)
   if (!validatedFormData.success) {
     return {
-      message: "Invalid form data.",
-    };
+      message: 'Invalid form data.',
+    }
   }
 
-  const { email, password } = validatedFormData.data;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { email, password } = validatedFormData.data
+  const hashedPassword = await bcrypt.hash(password, 10)
   try {
     await prisma.user.create({
       data: {
         email,
         hashedPassword,
       },
-    });
+    })
   } catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === "P2002") {
+      if (error.code === 'P2002') {
         return {
-          message: "Email already exists.",
-        };
+          message: 'Email already exists.',
+        }
       }
     }
 
     return {
-      message: "Could not create user.",
-    };
+      message: 'Could not create user.',
+    }
   }
 
-  await signIn("credentials", formData);
+  await signIn('credentials', formData)
 }
 
 export async function logOut() {
-  await signOut({ redirectTo: "/" });
+  await signOut({ redirectTo: '/' })
 }
 
 // --- pet actions ---
 
 export async function addPet(pet: unknown) {
-  const session = await checkAuth();
+  const session = await checkAuth()
 
-  const validatedPet = petFormSchema.safeParse(pet);
+  const validatedPet = petFormSchema.safeParse(pet)
   if (!validatedPet.success) {
     return {
-      message: "Invalid pet data.",
-    };
+      message: 'Invalid pet data.',
+    }
   }
 
   try {
@@ -115,42 +115,42 @@ export async function addPet(pet: unknown) {
           },
         },
       },
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return {
-      message: "Could not add pet.",
-    };
+      message: 'Could not add pet.',
+    }
   }
 
-  revalidatePath("/app", "layout");
+  revalidatePath('/app', 'layout')
 }
 
 export async function editPet(petId: unknown, newPetData: unknown) {
   // authentication check
-  const session = await checkAuth();
+  const session = await checkAuth()
 
   // validation
-  const validatedPetId = petIdSchema.safeParse(petId);
-  const validatedPet = petFormSchema.safeParse(newPetData);
+  const validatedPetId = petIdSchema.safeParse(petId)
+  const validatedPet = petFormSchema.safeParse(newPetData)
 
   if (!validatedPetId.success || !validatedPet.success) {
     return {
-      message: "Invalid pet data.",
-    };
+      message: 'Invalid pet data.',
+    }
   }
 
   // authorization check
-  const pet = await getPetById(validatedPetId.data);
+  const pet = await getPetById(validatedPetId.data)
   if (!pet) {
     return {
-      message: "Pet not found.",
-    };
+      message: 'Pet not found.',
+    }
   }
   if (pet.userId !== session.user.id) {
     return {
-      message: "Not authorized.",
-    };
+      message: 'Not authorized.',
+    }
   }
 
   // database mutation
@@ -160,39 +160,39 @@ export async function editPet(petId: unknown, newPetData: unknown) {
         id: validatedPetId.data,
       },
       data: validatedPet.data,
-    });
+    })
   } catch (error) {
     return {
-      message: "Could not edit pet.",
-    };
+      message: 'Could not edit pet.',
+    }
   }
 
-  revalidatePath("/app", "layout");
+  revalidatePath('/app', 'layout')
 }
 
 export async function deletePet(petId: unknown) {
   // authentication check
-  const session = await checkAuth();
+  const session = await checkAuth()
 
   // validation
-  const validatedPetId = petIdSchema.safeParse(petId);
+  const validatedPetId = petIdSchema.safeParse(petId)
   if (!validatedPetId.success) {
     return {
-      message: "Invalid pet data.",
-    };
+      message: 'Invalid pet data.',
+    }
   }
 
   // authorization check
-  const pet = await getPetById(validatedPetId.data);
+  const pet = await getPetById(validatedPetId.data)
   if (!pet) {
     return {
-      message: "Pet not found.",
-    };
+      message: 'Pet not found.',
+    }
   }
   if (pet.userId !== session.user.id) {
     return {
-      message: "Not authorized.",
-    };
+      message: 'Not authorized.',
+    }
   }
 
   // database mutation
@@ -201,38 +201,38 @@ export async function deletePet(petId: unknown) {
       where: {
         id: validatedPetId.data,
       },
-    });
+    })
   } catch (error) {
     return {
-      message: "Could not delete pet.",
-    };
+      message: 'Could not delete pet.',
+    }
   }
 
-  revalidatePath("/app", "layout");
+  revalidatePath('/app', 'layout')
 }
 
 // --- payment actions ---
 
 export async function createCheckoutSession() {
   // authentication check
-  const session = await checkAuth();
+  const session = await checkAuth()
 
-  console.log(session.user.email);
+  console.log(session.user.email)
 
   // create checkout session
   const checkoutSession = await stripe.checkout.sessions.create({
     customer_email: session.user.email,
     line_items: [
       {
-        price: "price_1OfpJ7FIW685mC8GCahpbCed",
+        price: 'price_1PSeZCGlr8ree41U04zLzQIZ',
         quantity: 1,
       },
     ],
-    mode: "payment",
+    mode: 'payment',
     success_url: `${process.env.CANONICAL_URL}/payment?success=true`,
     cancel_url: `${process.env.CANONICAL_URL}/payment?cancelled=true`,
-  });
+  })
 
   // redirect user
-  redirect(checkoutSession.url);
+  redirect(checkoutSession.url)
 }
